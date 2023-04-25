1c04cf62c9cabc814a43cf4c0b7f8007b4d3d689
Maksim Zakharchuk <mad_max0100@mail.ru>
2023-04-25 20:33:40 +0300

    feat: доработать контроллер createUser, создать контроллеры login и getUsersMe

--- controllers/users.js
+++ controllers/users.js
@@ -1,81 +1,106 @@
-const { ERROR_INCORRECT_DATA, ERROR_NOT_FOUND, ERROR_BY_DEFAULT } = require('../utils/constants');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../utils/constants');
+const customError = require('../errors');
 const User = require('../models/user');
 
 const findUser = (user, res) => {
-  if (user) {
-    return res.send(user);
+  if (!user) {
+    throw new customError.NotFoundError('Запрашиваемый пользователь не найден.');
   }
-  return res.status(ERROR_NOT_FOUND).send({ message: 'Запрашиваемый пользователь не найден.' });
+  return res.send(user);
 };
 
-const getUsers = (req, res) => {
+const getUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => {
-      res.status(ERROR_BY_DEFAULT).send({ message: 'На сервере произошла ошибка.' });
-    });
+    .catch(next);
 };
 
-const getUserById = (req, res) => {
+const getUsersMe = (req, res, next) => {
+  User.findById(req.user._id)
+    .then((user) => res.send(user))
+    .catch(next);
+};
+
+const getUserById = (req, res, next) => {
   const { userId } = req.params;
 
   User.findById(userId)
     .then((user) => findUser(user, res))
-    .catch((error) => {
-      if (error.name === 'CastError') {
-        return res.status(ERROR_INCORRECT_DATA).send({ message: 'Введен некорректный _id.' });
-      }
-      return res.status(ERROR_BY_DEFAULT).send({ message: 'На сервере произошла ошибка.' });
+    .catch(next);
+};
+
+const createUser = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => {
+      User.create({
+        email: req.body.email,
+        password: hash,
+        name: req.body.name,
+        about: req.body.about,
+        avatar: req.body.avatar,
+      })
+        .then((newUser) => {
+          res.status(201).send({
+            email: newUser.email,
+            name: newUser.name,
+            about: newUser.about,
+            avatar: newUser.avatar,
+          });
+        })
+        .catch((error) => {
+          if (error.code === 11000) {
+            next(new customError.EmailUseError('Пользователь с таким email уже существует.'));
+          }
+        });
     });
 };
 
-const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+const login = (req, res, next) => {
+  const { email, password } = req.body;
 
-  User.create({ name, about, avatar })
-    .then((newUser) => {
-      res.send(newUser);
+  User.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new customError.AuthFailedError('Введена неверная почта или пароль.');
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          next(new customError.AuthFailedError('Введена неверная почта или пароль.'));
+        }
+        const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '10d' });
+        return res.send({ token });
+      });
     })
-    .catch((error) => {
-      if (error.name === 'ValidationError') {
-        return res.status(ERROR_INCORRECT_DATA).send({ message: 'Переданы некорректные данные при создании профиля.' });
-      }
-      return res.status(ERROR_BY_DEFAULT).send({ message: 'На сервере произошла ошибка.' });
-    });
+    .catch(next);
 };
 
-const updateProfile = (req, res) => {
+const updateProfile = (req, res, next) => {
   const { name, about } = req.body;
   const owner = req.user._id;
 
   User.findByIdAndUpdate(owner, { name, about }, { new: true, runValidators: true })
     .then((user) => findUser(user, res))
-    .catch((error) => {
-      if (error.name === 'ValidationError') {
-        return res.status(ERROR_INCORRECT_DATA).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
-      }
-      return res.status(ERROR_BY_DEFAULT).send({ message: 'На сервере произошла ошибка.' });
-    });
+    .catch(next);
 };
 
-const updateAvatar = (req, res) => {
+const updateAvatar = (req, res, next) => {
   const avatar = req.body;
   const owner = req.user._id;
 
   User.findByIdAndUpdate(owner, avatar, { new: true, runValidators: true })
     .then((user) => findUser(user, res))
-    .catch((error) => {
-      if (error.name === 'ValidationError') {
-        return res.status(ERROR_INCORRECT_DATA).send({ message: 'Переданы некорректные данные при обновлении аватара.' });
-      }
-      return res.status(ERROR_BY_DEFAULT).send({ message: 'На сервере произошла ошибка.' });
-    });
+    .catch(next);
 };
 
 module.exports = {
   getUsers,
+  getUsersMe,
   getUserById,
   createUser,
+  login,
   updateProfile,
   updateAvatar
 };
