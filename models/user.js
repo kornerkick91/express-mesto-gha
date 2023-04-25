44a593cb42f179e6f3f0a07c4392c4158e75416f
Maksim Zakharchuk <mad_max0100@mail.ru>
2023-04-25 18:56:29 +0300

    feat: добавить email и password к схеме пользователя

--- models/user.js
+++ models/user.js
@@ -1,22 +1,45 @@
 const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
+const { urlRegex } = require('../utils/constants');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true
+    default: 'Жак-Ив Кусто'
   },
 
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true
+    default: 'Исследователь'
   },
 
   avatar: {
     type: String,
+    validate: {
+      validator: (v) => urlRegex.test(v),
+      message: 'Формат ссылки некорректен!'
+    },
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Формат почты некорректен!'
+    },
+    required: true
+  },
+
+  password: {
+    type: String,
+    minlength: 8,
+    select: false,
     required: true
   }
 });
